8fc477d1e326f5a8c33c6ee278f143608c9a4fdb
AnaCRgogni <106693889+AnaCRgogni@users.noreply.github.com>
2023-04-16 10:58:44 -0600

    assets_model como TIENE que estar por el momento
    Si falta algo, se agregara luego, ahorita vamos paso por paso, una cosa tiene que funcionar antes de movernos a la siguiente y estoy trabajando en generar el location code automaticamente al registrar un asset antes de cualquier otra cosa.

--- models/assets_model.js
+++ models/assets_model.js
@@ -29,16 +29,6 @@ const assetSchema = mongoose.Schema({
     type: String,
     required: true,
   },
-  createdBy: {
-    type: String,
-    required: true,
-  },
-  createdOn: {
-    type:Date,
-    default: Date.now,
-    required: true,
-    
-  },
 });
 
 const Asset = mongoose.model("Asset", assetSchema);
