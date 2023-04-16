bffaef97d05aa1c870fd664a4764d3cced16f236
angelo-oviedo <angelooviedomont22@gmail.com>
2023-04-16 17:25:24 -0600

    New password attribute for the users schema.

--- models/users_model.js
+++ models/users_model.js
@@ -33,6 +33,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model("User", userSchema);
