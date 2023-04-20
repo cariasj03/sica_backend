082e3f476fa32cefde0de94ef3dc03d8d3b3f2fe
AnaCRgogni <106693889+AnaCRgogni@users.noreply.github.com>
2023-04-19 22:43:35 -0600

    Minor edits to help front end
    functionality of the asset registration html and js

--- routes/assets_route.js
+++ routes/assets_route.js
@@ -1,21 +1,22 @@
 //Requiring modules
-const fs = require('fs');
-const express = require('express');
-const assetsModel = require('../models/assets_model');
+const fs = require("fs");
+const express = require("express");
+const assetsModel = require("../models/assets_model");
+const nextId = require("../bl/next_id");
+const { request } = require("http");
 
 //Creating app
 const app = express();
 app.use(express.json());
 
 //Adding a new asset
-app.post('/assets', async (req, res) => {
-  const asset = new assetsModel(req.body);
-
+app.post("/assets", async (req, res) => {
   try {
-    console.log('Attending the POST route: /assets');
+    console.log("Attending the POST route: /assets");
+    const asset = new assetsModel(req.body);
 
     await asset.save();
-    console.log('Activo creado:', asset);
+    console.log("Activo creado:", asset);
 
     res.status(201).send(asset);
   } catch (error) {
@@ -24,7 +25,7 @@ app.post('/assets', async (req, res) => {
   }
 });
 
-app.get('/assets', async (req, res) => {
+app.get("/assets", async (req, res) => {
   try {
     const assets = await assetsModel.find({});
     res.send(assets);
