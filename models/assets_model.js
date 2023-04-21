48bda522a44110096569f33bf6a7cc45c15e434b
AnaCRgogni <106693889+AnaCRgogni@users.noreply.github.com>
2023-04-20 18:23:38 -0600

    Added ID auto creation to assets

--- models/assets_model.js
+++ models/assets_model.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 
 const assetSchema = mongoose.Schema({
-  // id: {
-  //   type: String,
-  //   required: true,
-  // },
+  id: {
+    type: String,
+    required: true,
+  },
   name: {
     type: String,
     required: true,
--- routes/assets_route.js
+++ routes/assets_route.js
@@ -1,22 +1,32 @@
 //Requiring modules
-const fs = require("fs");
 const express = require("express");
 const assetsModel = require("../models/assets_model");
 const nextId = require("../bl/next_id");
-const { request } = require("http");
+const cors = require("cors");
 
 //Creating app
 const app = express();
 app.use(express.json());
+app.use(cors({}));
 
 //Adding a new asset
 app.post("/assets", async (req, res) => {
   try {
-    console.log("Attending the POST route: /assets");
-    const asset = new assetsModel(req.body);
+    //Adding id and creationDate to the asset
+    const assetJson = req.body;
+    const nextAssetId = await nextId.getAssetId();
+    assetJson.id = nextAssetId;
 
+    //Creating asset model with new asset info
+    const asset = new assetsModel(assetJson);
+
+    console.log("Attending the POST route: /assets");
+
+    console.log(assetJson);
+    console.log(asset);
     await asset.save();
-    console.log("Activo creado:", asset);
+
+    console.log("Activo creado", asset);
 
     res.status(201).send(asset);
   } catch (error) {
@@ -28,6 +38,17 @@ app.post("/assets", async (req, res) => {
 app.get("/assets", async (req, res) => {
   try {
     const assets = await assetsModel.find({});
+    res.send(assets);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+//Fetching all assets by id
+app.get("/assets/sort/by-id", async (req, res) => {
+  try {
+    console.log("Attending the GET route: /assets/sort/by-id");
+    const assets = await assetsModel.find().sort({ id: 1 }).exec();
     res.send(assets);
   } catch (error) {
     res.status(500).send(error);
